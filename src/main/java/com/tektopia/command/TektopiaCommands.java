87d6b3aef9654c28f17467a2497fd638af3d5090
Hengku318 <hengku568@gmail.com>
2026-09-21 00:54:12 +0700

    Update TektopiaCommands.java

--- src/main/java/com/tektopia/command/TektopiaCommands.java
+++ src/main/java/com/tektopia/command/TektopiaCommands.java
@@ -6,7 +6,7 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.npc.villager.Villager;
 
 import java.util.List;
 
